5b628d310bcaed51e9742fe67c732ec9f374aa75
Anton Golomazov <golomazov-tosha2012@yandex.ru>
2019-04-07 15:32:00 +0300

    Setup redux and add tests for successReducer and actions

---
+++ src/configureStore.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './reducers';
+
+const composeEnhancers =
+  typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(),
+);
+
+export default createStore(rootReducer, enhancer);
